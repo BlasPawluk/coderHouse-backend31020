3100b1d64894248e1d4c6e39c737470b866ff9da
Blas Pawluk <blaspaw99@gmail.com>
2022-06-29 09:30:43 -0300

    ultimos cambios

--- servidor.js
+++ servidor.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const Contenedor = require('./index2.js');
 //Pase todos los productos a productos.txt
-//let productos = [
-//  { id: 1, nombre: 'Laptop', precio: 1000 },
-//  { id: 2, nombre: 'Celular', precio: 2000 },
-//  { id: 3, nombre: 'Tablet', precio: 3000 },];
+let productos = [
+  { id: 1, nombre: 'Laptop', precio: 1000 },
+  { id: 2, nombre: 'Celular', precio: 2000 },
+  { id: 3, nombre: 'Tablet', precio: 3000 },
+];
 
 app.get('/productos', (req, resp) => {
-  fs.readFile('productos.txt', 'utf8', (err, data) => {
-    let productos = JSON.parse(data);
-    resp.send(productos);
-  });
+  resp.send(productos);
 });
 
 app.get('/productosRandom/:id', async (req, resp) => {
-  let id = Number(req.params.id);
+  let id = Number(req.params.id) - 1;
   let result = await fs.promises.readFile('productos.txt', 'utf-8');
   let productos = JSON.parse(result);
   let buscador = buscarProducto(productos, id);
@@ -29,5 +26,5 @@ const buscarProducto = (productos, id) => {
   return prod;
 };
 
-const PORT = 8000;
-app.listen(PORT, () => console.log('Server on port 8000'));
+const PORT = 8080;
+app.listen(PORT, () => console.log('Server on port 8080'));
